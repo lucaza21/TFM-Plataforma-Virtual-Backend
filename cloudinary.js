c128f132f8f7a7dd664cb38971e7c2003d32f220
lucaza21 <luis.carrasquilla.z@gmail.com>
2024-03-11 15:19:15 -0500

    catcursos añade ruta archivo de presentación a propiedad insumos de tabla

--- cloudinary.js
+++ cloudinary.js
@@ -9,6 +9,7 @@ cloudinary.config({
     secure: true
 });
 
+//upload
 const uploadFolder = async (folderName, fName) => {
     return await cloudinary.uploader.upload('uploads\\cd1fd1dec3de7d429ac794bedddd1f2f', 
         {
